3dce3495474aba8ff8dd8f16a284901b1fb65d1d
@MikhailYurikov <mishanya-yurikov@yandex.ru>
2025-06-17 19:18:19 +1000

    1

---
+++ src/task2/Main.java
@@ -0,0 +1,13 @@
+package task2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int income = 44000;
+        int spending = 30000;
+
+        System.out.println("Итого (руб):");
+        System.out.print(income - spending);
+
+    }
+}
